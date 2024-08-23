4bd2e94a51c454b7994ffa1609d58265a6fd5840
DawonOh <dawonohh@gmail.com>
2024-08-23 19:39:04 +0900

    feat: header메뉴 깃허브 링크 추가 - menuList.json 파일 추가 - header.tsx 반복되는 코드 수정

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 import MobileMenu from "../mobileMenu/mobileMenu";
 
+export interface MenuListType {
+  id: number;
+  menu: string;
+  link: string;
+}
+
 export default function Header() {
+  const menuList = require("/public/data/menuList.json");
   return (
     <header className="sticky top-0 p-4 flexCenter md:flex-col bg-white/60 backdrop-blur-xl border-b border-medium-gray z-50">
       <ul className="md:hidden block w-1/2 md:w-5/4 flex justify-between items-center">
-        <li>
-          <Link href="/#home">Home</Link>
-        </li>
-        <li>
-          <Link href="/#about">About</Link>
-        </li>
-        <li>
-          <Link href="/#skills">Skills</Link>
-        </li>
-        <li>
-          <Link href="/#projects">Projects</Link>
-        </li>
+        {menuList.map((menu: MenuListType) => (
+          <li key={menu.id} className="hover:text-main-blue">
+            <Link href={menu.link}>{menu.menu}</Link>
+          </li>
+        ))}
       </ul>
       <MobileMenu />
     </header>
--- app/components/mobileMenu/mobileMenu.tsx
+++ app/components/mobileMenu/mobileMenu.tsx
@@ -6,10 +6,11 @@ import { useState } from "react";
 import menuIcon from "@/public/assets/icon/menu.png";
 import menuCloseIcon from "@/public/assets/icon/menu_close.png";
 import Link from "next/link";
+import { MenuListType } from "../header/header";
 
 export default function MobileMenu() {
   const [isMenuOn, setIsMenuOn] = useState(false);
-  const menuList = ["Home", "About", "Skills", "Projects"];
+  const menuList = require("/public/data/menuList.json");
   return (
     <>
       <div className="hidden md:flex w-full h-full justify-end items-center cursor-pointer z-50">
@@ -24,9 +25,15 @@ export default function MobileMenu() {
       {isMenuOn && (
         <div className="hidden w-full h-screen md:flexCenter md:fixed md:top-0 backdrop-blur-xl bg-white/90">
           <ul className="flexCenter flex-col gap-4 text-2xl font-semibold">
-            {menuList.map((str: string, idx) => (
-              <li key={idx} className="hover:text-main-blue">
-                <Link href={`/#${str.toLowerCase()}`}>{str}</Link>
+            {menuList.map((menu: MenuListType) => (
+              <li
+                key={menu.id}
+                className="hover:text-main-blue"
+                onClick={() => {
+                  setIsMenuOn(false);
+                }}
+              >
+                <Link href={menu.link}>{menu.menu}</Link>
               </li>
             ))}
           </ul>
